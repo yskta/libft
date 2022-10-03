3aa879bfd165ea5b80eeda2ec96971e850e31bd6
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-10-03 20:15:52 +0900

    commit

---
+++ ft_isalnum.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+int ft_isalpha(int c)
+{
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+        return (1);
+    else
+        return (0);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+int	ft_isalnum(int c)
+{
+	if ((ft_isalpha(c) != 0) || (ft_isdigit(c) != 0))
+		return (1);
+	else
+		return (0);
+}
