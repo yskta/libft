6f9d7016fb23367d227256dd09f0274c575d9210
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-10-05 22:59:20 +0900

    commit

--- main_isalpha.c
+++ main_isalpha.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "libft.h"
 
 int main(void)
 {
+    printf("result of ft_isalpha\n");
     printf("a：%d\n", ft_isalpha('a'));
+    printf("1：%d\n", ft_isalpha('1'));
     printf("1234567：%d\n", ft_isalpha(1234567));
+    printf("====================\n");
+    printf("result of isalpha\n");
+    printf("a：%d\n", isalpha('a'));
+    printf("1：%d\n", isalpha('1'));
+    printf("1234567：%d\n", isalpha(1234567));
     return (0);
 }
--- main_isascii.c
+++ main_isascii.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "libft.h"
 
 int main(void)
 {
+    printf("result of ft_isalpha\n");
     printf("a：%d\n", ft_isascii('a'));
-    printf("9：%d\n", ft_isascii(9));
+    printf("9：%d\n", ft_isascii('9'));
     printf("1234567：%d\n", ft_isascii(1234567));
     printf("!：%d\n", ft_isascii('!'));
+    printf("====================\n");
+    printf("result of isalpha\n");
+    printf("a：%d\n", isascii('a'));
+    printf("9：%d\n", isascii('9'));
+    printf("1234567：%d\n", isascii(1234567));
+    printf("!：%d\n", isascii('!'));
     return (0);
 }
