a919022a8576c6e08e0b500b990e724d4d2a44d1
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-10-08 16:27:03 +0900

    commit

--- main_ft_putnbr_fd.c
+++ main_ft_putnbr_fd.c
@@ -7,11 +7,15 @@ int main(void)
     printf("<result of ft_putnbr_fd>\n");
     ft_putnbr_fd(-2147483648, 1);
     printf("\n");
-    printf("<n2>\n0\n");
+    printf("<n2>\n-10\n");
+    printf("<result of ft_putnbr_fd>\n");
+    ft_putnbr_fd(-10, 1);
+    printf("\n");
+    printf("<n3>\n0\n");
     printf("<result of ft_putnbr_fd>\n");
     ft_putnbr_fd(0, 1);
     printf("\n");
-    printf("<n3>\n2147483647\n");
+    printf("<n4>\n2147483647\n");
     printf("<result of ft_putnbr_fd>\n");
     ft_putnbr_fd(2147483647, 1);
     printf("\n");
