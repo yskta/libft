7154875a72dfeada1a6e4cd542d59b12d91421e1
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-10-08 21:03:03 +0900

    commit

---
+++ ft_striteri.c
@@ -0,0 +1,13 @@
+
+#include "libft.h"
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    size_t i;
+
+    i = 0;
+    if (s[i] == '\0' || !f)
+        return ;
+    while (s[i++] != '\0')
+        (*f)(i, &s[i]);
+}
