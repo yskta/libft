48f842d8b6f5d37a38a63e51429b789c549166bf
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-10-08 13:04:41 +0900

    commit

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,12 +1,40 @@
+/*
+size バイトのブロックを n個割り当てます。確保されたブロックは 0 クリアされます。→bzero関数使う
+この関数を使うことにより、プログラムで予め配列などを使ってメモリを確保せずに、必要な場所で必要なだけメモリを確保することが出来ます。
+【引数】
+size_t size　：　確保したいメモリのバイトサイズ
+　　　　　　　　　size_t は stdlib.h の中でtypedefされ、一般にunsignedです。
+【戻り値】
+成功時　：　確保したメモリブロックを指すポインタ　（void *についてはmalloc関数参照）
+失敗時　：　NULL　（メモリ不足により指定サイズ分のメモリが確保できないとき）
+*/
 #include "libft.h"
+
+void    *ft_memset(void *s, int c, size_t n)
+{
+    char *d;
+
+    d = (char *)s;
+    while (n--)
+    {
+        *d = c;
+        d++;
+    }
+    return (s);
+}
+
+void    ft_bzero(void *s, size_t n)
+{
+    ft_memset(s, '\0', n);
+}
 
 void	*ft_calloc(size_t number, size_t size)
 {
-	void	*ptr;
+	void	*p;
 
-	ptr = malloc(number * size);
-	if (ptr == NULL)
+	p = malloc(number * size);
+	if (p == NULL)
 		return (NULL);
-	ft_bzero(ptr, (number * size));
-	return (ptr);
+	ft_bzero(p, (number * size));
+	return (p);
 }
--- main_calloc.c
+++ main_calloc.c
@@ -1,8 +1,29 @@
-#include <stdlib.h>
+/*callocのテストは積み残し*/
+#include <string.h>
 #include <stdio.h>
 #include "libft.h"
 
 int main(void)
 {
-    
+    printf("result of ft_calloc\n");
+    ft_calloc(0, 0);
+    ft_calloc(1, 0);
+    ft_calloc(0, 1);
+    ft_calloc(1, 1);
+    ft_calloc(42, 1);
+    ft_calloc(1, 42);
+    ft_calloc(42, 42);
+    ft_calloc((size_t)SIZE_MAX / 10 + (size_t)1, 10);
+
+    printf("====================\n");
+    printf("result of calloc\n");
+    calloc(0, 0);
+    calloc(1, 0);
+    calloc(0, 1);
+    calloc(1, 1);
+    calloc(42, 1);
+    calloc(1, 42);
+    calloc(42, 42);
+    calloc((size_t)SIZE_MAX / 10 + (size_t)1, 10);
+    return 0;
 }
